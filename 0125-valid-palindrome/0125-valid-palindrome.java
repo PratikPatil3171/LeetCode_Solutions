d39469fa28aa9d3c42a9fb5b67f2e435dc0c7ff2
Pratik Patil <pratikjp.290606@gmail.com>
2026-09-26 09:35:23 +0530

    Time: 2 ms (99.29%), Space: 44.4 MB (53.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,47 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if(s.length() == 0) return true;
-        s = s.toLowerCase();
-        String filterStr = s.toLowerCase().replaceAll("[^a-z0-9]","");
-       return palindrome(0,filterStr);
+       // BRUTE - REVERSE STRING AND THEN CHECK EQUAL OR NOT
+    //    StringBuilder filtered = new StringBuilder();
+
+    //     for (int i = 0; i < s.length(); i++) {
+    //         char ch = s.charAt(i);
+    //         if (Character.isLetterOrDigit(ch)) {
+    //             filtered.append(Character.toLowerCase(ch));
+    //         }
+    //     }
+
+    //     String original = filtered.toString();
+    //     String reversed = filtered.reverse().toString();
+
+    //     return original.equals(reversed);
+
+    //OPTIMAL - TWO POINTER
+    int left=0;
+    int right = s.length()-1;
+    while(left<right){
+        if(!Character.isLetterOrDigit(s.charAt(left))){
+            left++;
+        }
+        else if(!Character.isLetterOrDigit(s.charAt(right))){
+            right--;
+        }else{
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+    }
+    return true;
+
+
         
     }
-    public boolean palindrome(int i,String s){
-        if(i>=s.length()/2) return true;
-        if(s.charAt(i)!=s.charAt(s.length()-i-1)) return false;
+    // public boolean palindrome(int i,String s){
+    //     if(i>=s.length()/2) return true;
+    //     if(s.charAt(i)!=s.charAt(s.length()-i-1)) return false;
 
-        return palindrome(i+1,s);
+    //     return palindrome(i+1,s);
 
-    }
+    // }
 }
